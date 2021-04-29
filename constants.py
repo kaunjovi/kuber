7b58ca46fdc383774430f4216934c3359fd4f3e6
kaun jovi <kaunjovi@gmail.com>
2021-04-29 23:43:15 +0530

    three constants

--- constants.py
+++ constants.py
@@ -16,5 +16,26 @@ def get_full_path_of_raw_data_file ( date_string) :
     return CONSTANTS()['sec_bhavdata_folder'] + date_string + '.csv'
 
 
+def get_data_folder() : 
+    return '/Users/kaunjovi/code/kuber/data/'
+
+def get_sec_bhavdata_file_name (date_string) : 
+    return get_data_folder() + '100_full_bhav_copy/sec_bhavdata_full_'+date_string+'.csv'
+
+def get_sec_bhavdata_url (date_string) : 
+    return 'https://archives.nseindia.com/products/content/sec_bhavdata_full_' + date_string + '.csv' 
+    
+def get_top_delivery_file_name(date_string) : 
+    return get_data_folder() + '200_top_daily_deliveries/top_deliveries_'+date_string+'.csv'
+
+def get_top_average_delivery_file_name () : 
+    return get_data_folder() + '300_top_daily_average/top_average_deliveries.csv'
+
+def get_top_deliveries_file_quoted_first_step() : 
+    return get_data_folder() + '400_js_data/top_deliveries_quoted_1.csv'
+
+def get_top_deliveries_js_data_file() : 
+    return '/Users/kaunjovi/code/kuber-online/data.js' 
+
 # if __name__ == "__main__":
 #     print (CONSTANTS()['sec_bhavdata_url'])
