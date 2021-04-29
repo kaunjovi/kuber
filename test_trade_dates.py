fce42e31731dc925b880bc126fe26680c8094db8
kaun jovi <kaunjovi@gmail.com>
2021-04-29 23:40:59 +0530

    moved the test scripts to their own folder

--- test_trade_dates.py
+++
@@ -1,5 +0,0 @@
-from trade_dates import fetch_trade_date_strings 
-
-def test_list_of_recent_dates(): 
-    trade_date_strings = fetch_trade_date_strings('recent.dates')
-    assert len(trade_date_strings) == 12 
