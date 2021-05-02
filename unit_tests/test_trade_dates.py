eac2d08d8dd9e008cff26595eaedb98cc6061b68
kaun jovi <kaunjovi@gmail.com>
2021-05-02 10:02:55 +0530

    get rid of a few.

--- unit_tests/test_trade_dates.py
+++
@@ -1,8 +0,0 @@
-import sys, os
-sys.path.append(os.path.realpath(os.path.dirname(__file__)+"/.."))
-
-from trade_dates import fetch_trade_date_strings 
-
-def test_list_of_recent_dates(): 
-    trade_date_strings = fetch_trade_date_strings('recent.dates')
-    assert len(trade_date_strings) == 17 
